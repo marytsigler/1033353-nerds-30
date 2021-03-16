3390c5ef169b396eedefd5f8d4bef53b02c870e3
marytsigler <77626854+marytsigler@users.noreply.github.com>
2021-03-16 11:47:28 +0300

    Доступная разметка внутренней страницы

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,58 @@
-const contactsButton = document.querySelector(".contacts-button");
-const modalForm = document.querySelector(".modal-form");
-const popupClose = document.querySelector(".popup-button-close");
+const contactsButton = document.querySelector(".contacts-button");/*находит кнопку по которой открывается попап*/
+const modalForm = document.querySelector(".modal-form");/*само модальное окно - секция в HTML*/
+const popupClose = modalForm.querySelector(".popup-button-close");
+const formPopup = modalForm.querySelector("formPopup");/*Форма */
+const modalUser = modalForm.querySelector(".modal-user");/*Инпут Имя*/
+const modalEmail = modalForm.querySelector(".modal-email");/*Инпут Емэйл*/
 
+let isStorageSupport = true;
+let storage = "";
 
+try	{
+	storage = localStorage.getItem("name");
+} catch (err){
+	isStorageSupport = false;
+}
+
+/*Открывает форму*/
 
 contactsButton.addEventListener("click", function(evt){
-	evt.preventDefault();	/*отключаем переход на другую страницу*/
+	evt.preventDefault();	/*отключает переход на другую страницу*/
 	modalForm.classList.add("modal-form-show");/*показывает форму*/
+
+	if (storage) {
+		modalUser.value	= storage;
+		modalEmail = focus();
+	}
+	else{
+		modalUser.focus();/*фокус при открытии модального окна в поле ввода логина.*/
+	}
 });
 
+/*Закрывает форму*/
 
 popupClose.addEventListener("click", function(evt){
-	evt.preventDefault();/*отключаем переход на другую страницу	*/
-	popupClose.classList.remove("modal-form-show");
+	evt.preventDefault();/*отключает переход на другую страницу	*/
+	modalForm.classList.remove("modal-form-show");/*отключает класс - закрывает форму*/
+});
+
+formPopup.addEventListener("submit", function(evt){
+	if (!modalUser.value || !modalEmail.value) {
+		evt.preventDefault();
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("name", modalUser.value);/*сохраняет имя*/
+		}	
+	}
+});
+
+/*Закрывает форму при нажатии esc*/
+
+window.addEventListener("keydown", function(evt){
+	if (evt.keyCode === 27) {
+		if (modalForm.classList.contains("modal-form-show")) {
+			evt.preventDefault();
+			modalForm.classList.remove("modal-form-show");
+		}
+	}
 });
